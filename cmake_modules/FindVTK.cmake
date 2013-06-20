aa383b371eee9bd0d77efa2dbe3cc114c24f2882
James E. Butler <jebutler@limitpoint.com>
2013-06-20 10:22:24 -0700

    Added code to FindVTK so user could choose vtk 5.6 or 5.10.

--- cmake_modules/FindVTK.cmake
+++ cmake_modules/FindVTK.cmake
@@ -56,6 +56,8 @@
 # - Check in the LPS standard prerequisites location.
 #=============================================================================
 
+set(VERSION 5.6)
+#set(VERSION 5.10)
 # Assume not found.
 set(VTK_FOUND 0)
 
@@ -71,7 +73,7 @@ if(NOT VTK_FOUND)
                    HINTS $ENV{HOME}/LPS/prerequisites/vtk $ENV{USERPROFILE}/LPS/prerequisites/vtk/lib)
 endif()
 
-
+if(VERSION MATCHES "5.10")
 
   # Look for UseVTK.cmake in build trees or under <prefix>/include/vtk.
   find_path(VTK_LIB_DIR
@@ -100,18 +102,55 @@ endif()
     # Help the user find it if we cannot.
     DOC "The ${VTK_DIR_DESCRIPTION}"
     )
+    get_filename_component(__TMP_DIR "${VTK_LIB_DIR}" PATH)
+    get_filename_component(__TMP_DIR "${__TMP_DIR}" PATH)
+    set(VTK_BIN_DIR "${__TMP_DIR}/bin" CACHE PATH "VTK Runtime libraries location.")
+    
+    get_filename_component(__TMP_DIR "${VTK_LIB_DIR}" PATH)
+    get_filename_component(__TMP_DIR "${__TMP_DIR}" PATH)
+    set(VTK_INCLUDE_DIRS "${__TMP_DIR}/include/vtk-5.10" CACHE PATH "VTK Headers location.")
 
-get_filename_component(__TMP_DIR "${VTK_LIB_DIR}" PATH)
-get_filename_component(__TMP_DIR "${__TMP_DIR}" PATH)
-set(VTK_BIN_DIR "${__TMP_DIR}/bin" CACHE PATH "VTK Runtime libraries location.")
+else()    
 
-get_filename_component(__TMP_DIR "${VTK_LIB_DIR}" PATH)
-get_filename_component(__TMP_DIR "${__TMP_DIR}" PATH)
-set(VTK_INCLUDE_DIRS "${__TMP_DIR}/include/vtk-5.10" CACHE PATH "VTK Headers location.")
+  # Look for UseVTK.cmake in build trees or under <prefix>/include/vtk.
+  find_path(VTK_LIB_DIR
+    NAMES UseVTK.cmake
+    PATH_SUFFIXES vtk-5.6 vtk
+    HINTS $ENV{VTK_LIB_DIR} $ENV{HOME}/LPS/prerequisites/vtk/lib $ENV{USERPROFILE}/LPS/prerequisites/vtk/lib
 
-#if(VTK_LIB_DIR)
-#    set(VTK_FOUND 1)
-#endif()
+    PATHS
+
+    # Support legacy cache files.
+    ${VTK_DIR_SEARCH_LEGACY}
+
+    # Read from the CMakeSetup registry entries.  It is likely that
+    # VTK will have been recently built.
+    [HKEY_CURRENT_USER\\Software\\Kitware\\CMakeSetup\\Settings\\StartPath;WhereBuild1]
+    [HKEY_CURRENT_USER\\Software\\Kitware\\CMakeSetup\\Settings\\StartPath;WhereBuild2]
+    [HKEY_CURRENT_USER\\Software\\Kitware\\CMakeSetup\\Settings\\StartPath;WhereBuild3]
+    [HKEY_CURRENT_USER\\Software\\Kitware\\CMakeSetup\\Settings\\StartPath;WhereBuild4]
+    [HKEY_CURRENT_USER\\Software\\Kitware\\CMakeSetup\\Settings\\StartPath;WhereBuild5]
+    [HKEY_CURRENT_USER\\Software\\Kitware\\CMakeSetup\\Settings\\StartPath;WhereBuild6]
+    [HKEY_CURRENT_USER\\Software\\Kitware\\CMakeSetup\\Settings\\StartPath;WhereBuild7]
+    [HKEY_CURRENT_USER\\Software\\Kitware\\CMakeSetup\\Settings\\StartPath;WhereBuild8]
+    [HKEY_CURRENT_USER\\Software\\Kitware\\CMakeSetup\\Settings\\StartPath;WhereBuild9]
+    [HKEY_CURRENT_USER\\Software\\Kitware\\CMakeSetup\\Settings\\StartPath;WhereBuild10]
+
+    # Help the user find it if we cannot.
+    DOC "The ${VTK_DIR_DESCRIPTION}"
+    )
+    get_filename_component(__TMP_DIR "${VTK_LIB_DIR}" PATH)
+    get_filename_component(__TMP_DIR "${__TMP_DIR}" PATH)
+    set(VTK_BIN_DIR "${__TMP_DIR}/bin" CACHE PATH "VTK Runtime libraries location.")
+    
+    get_filename_component(__TMP_DIR "${VTK_LIB_DIR}" PATH)
+    get_filename_component(__TMP_DIR "${__TMP_DIR}" PATH)
+    set(VTK_INCLUDE_DIRS "${__TMP_DIR}/include/vtk-5.6" CACHE PATH "VTK Headers location.")
+endif()
+
+if(VTK_LIB_DIR)
+    set(VTK_FOUND 1)
+endif()
 
 #-----------------------------------------------------------------------------
 if(VTK_FOUND)
