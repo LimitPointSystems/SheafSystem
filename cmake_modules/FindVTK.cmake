8ed08399c099a9f67938c29cb5846a35c2da58f3
James E. Butler <j.e.butler@limitpoint.com>
2013-06-27 10:48:56 -0700

    Rermoved debug printouts. Bamboo is apparently unable to resolve the value of $HOME on linux unless it is explicitly stipulated in the config file for the plan. Something not quite right about that, but it's a story for another time.

--- cmake_modules/FindVTK.cmake
+++ cmake_modules/FindVTK.cmake
@@ -113,10 +113,6 @@ if(VERSION MATCHES "5.10")
     get_filename_component(__TMP_DIR "${VTK_LIB_DIR}" PATH)
     get_filename_component(__TMP_DIR "${__TMP_DIR}" PATH)
     set(VTK_INCLUDE_DIRS "${__TMP_DIR}/include/vtk-5.10" CACHE PATH "VTK Headers location.")
-message(STATUS "${VTK_LIB_DIR}")
-message(STATUS "${__TMP_DIR}/include/vtk-5.10")
-message(STATUS "HOME dir is: $ENV{HOME}")
-message(STATUS "$ENV{HOME}/LPS/prerequisites/vtk/lib")
 else()    
 
   # Look for UseVTK.cmake in build trees or under <prefix>/include/vtk.
