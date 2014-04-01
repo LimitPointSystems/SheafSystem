12feda73d00673a0587095621c32ba7bb184cee8
James E. Butler <j.e.butler@limitpoint.com>
2014-04-01 10:35:37 -0700

    A few more changes.

--- cmake_modules/component_functions.cmake
+++ cmake_modules/component_functions.cmake
@@ -634,21 +634,6 @@ function(showincs)
     message(STATUS "===================================================")       
 endfunction()
 
-# 
-# Get the current system date and store it in RESULT
-#
-macro(get_date RESULT)
-    if(WIN32)
-        execute_process(COMMAND "cmd" " /C date /T" OUTPUT_VARIABLE ${RESULT})
-        string(REGEX REPLACE "(..)/(..)/..(..).*" "\\1/\\2/\\3" ${RESULT} ${${RESULT}})
-    elseif(UNIX)
-        execute_process(COMMAND "date" "+%m/%d/%Y" OUTPUT_VARIABLE ${RESULT})
-        string(REGEX REPLACE "(..)/(..)/..(..).*" "\\1/\\2/\\3" ${RESULT} ${${RESULT}})
-    else(WIN32)
-        message(WARNING "date not implemented")
-        set(${RESULT} 000000)
-    endif(WIN32)
-endmacro(get_date)
 
 # 
 # Install any prerequisites that need to ship with our libs
